e4f75cc17b37c083fda9523162b5a21cb09b6349
Carlos <20067643+Cabodevel@users.noreply.github.com>
2023-09-23 10:42:44 +0200

    Agregar archivos de proyecto.

---
+++ DesignPatterns/Creational/Singleton/Implementation.cs
@@ -0,0 +1,47 @@
+﻿namespace Singleton;
+
+public class Logger
+{
+    private static Logger _instance;
+
+    protected Logger()
+    {
+    }
+
+    public static Logger Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = new Logger();
+            }
+            return _instance;
+        }
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+public class ThreadSafeLogger
+{
+    private static readonly Lazy<ThreadSafeLogger> _threadSafeInstance =
+        new Lazy<ThreadSafeLogger>(() => new ThreadSafeLogger());
+
+    protected ThreadSafeLogger()
+    {
+    }
+
+    public static ThreadSafeLogger Instance
+    {
+        get => _threadSafeInstance.Value;
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
---
+++ DesignPatterns/Creational/Singleton/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Singleton;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Singleton pattern");
+
+        var instance1 = Logger.Instance;
+        var instance2 = Logger.Instance;
+
+        instance1.Log("Message from instance 1");
+        instance2.Log("Message from instance 2");
+        Console.WriteLine("Are same instance = {0}", instance1 == instance2);
+
+        Console.WriteLine("Thread safe logger singleton");
+
+        var tsinstance1 = ThreadSafeLogger.Instance;
+        var tsinstance2 = ThreadSafeLogger.Instance;
+
+        tsinstance1.Log("Message from ThreadSafeLogger 1");
+        tsinstance2.Log("Message from ThreadSafeLogger 2");
+
+        Console.WriteLine("ThreadSafeLogger Are same instance = {0}", tsinstance1 == tsinstance2);
+    }
+}
