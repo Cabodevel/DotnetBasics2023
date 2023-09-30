aef3c442926838682349c7b036e3ea1310197834
Carlos <20067643+Cabodevel@users.noreply.github.com>
2023-09-30 20:17:51 +0200

    add prototype

---
+++ DesignPatterns/Creational/Prototype/Implementation.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Prototype;
+
+//prototype
+public abstract class Person
+{
+    public abstract string Name { get; set; }
+
+    public abstract Person Clone();
+
+    public abstract Person DeepClone();
+}
+
+public class Manager : Person
+{
+    [JsonConstructor]
+    public Manager(string name)
+    {
+        Name = name;
+    }
+
+    public override string Name { get; set; }
+
+    public override Person Clone() => (Person)MemberwiseClone();
+
+    public override Person DeepClone()
+    {
+        var jsonString = JsonSerializer.Serialize(this);
+        return JsonSerializer.Deserialize<Manager>(jsonString);
+    }
+}
+
+public class Employee : Person
+{
+    [JsonConstructor]
+    public Employee(Manager manager, string name)
+    {
+        Manager = manager;
+        Name = name;
+    }
+
+    public Manager Manager { get; set; }
+    public override string Name { get; set; }
+
+    public override Person Clone() => (Person)MemberwiseClone();
+
+    public override Person DeepClone()
+    {
+        var jsonString = JsonSerializer.Serialize(this);
+        return JsonSerializer.Deserialize<Employee>(jsonString);
+    }
+}
--- DesignPatterns/Creational/Prototype/Program.cs
+++ DesignPatterns/Creational/Prototype/Program.cs
@@ -2,8 +2,41 @@
 
 internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        unsafe
+        {
+            var manager = new Manager("John Doe");
+            var managerClone = manager.Clone();
+            var managerPointer = &manager;
+            var managerClonePointer = &managerClone;
+
+            Console.WriteLine(manager.Name);
+            Console.WriteLine(managerClone.Name);
+            Console.WriteLine((int)managerPointer);
+            Console.WriteLine((int)managerClonePointer);
+
+            var employee = new Employee(manager, "Kevin");
+            var employeeClone = (Employee)employee.Clone();
+            Console.WriteLine("Shallow copy");
+            Console.WriteLine(employeeClone.Name);
+            Console.WriteLine(employeeClone.Manager.Name);
+
+            employeeClone.Manager.Name = "ChangedName";
+            //with shallow copy employee manager refers to manager
+            Console.WriteLine(manager.Name);
+
+            manager = new Manager("John Doe");
+
+            Console.WriteLine("Deep clone");
+
+            var deepClonedManager = manager.DeepClone();
+
+            deepClonedManager.Name = "new name";
+
+            //with deep clone managers are different refences
+            Console.WriteLine(manager.Name);
+            Console.WriteLine(deepClonedManager.Name);
+        }
     }
 }
