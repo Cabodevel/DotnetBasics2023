5e63169a7ab2a1c955bd5fd037df97d8d2f2c84c
Carlos <20067643+Cabodevel@users.noreply.github.com>
2023-10-07 12:21:41 +0200

    Facade

---
+++ DesignPatterns/Structural/Facade/Implementation.cs
@@ -0,0 +1,50 @@
+﻿namespace Facade;
+
+public class OrderService
+{
+    public bool HasEnoughOrders(int customerId)
+    {
+        //Testing purpose
+        return customerId > 5;
+    }
+}
+
+public class CustomerDiscountBaseService
+{
+    public double CalculateDiscountBase(int customerId)
+    {
+        //Testing purpose
+        return customerId > 8 ? 10 : 20;
+    }
+}
+
+public class DayOfTheWeekFactorService
+{
+    public double CalculateDayOfTheWeekFactor() =>
+        DateTime.UtcNow.DayOfWeek switch
+        {
+            DayOfWeek.Saturday => 0.8,
+            DayOfWeek.Sunday => 0.8,
+            _ => 1.2
+        };
+}
+
+public class DiscountFacade
+{
+    //Add DI When configure Container
+    private readonly OrderService _orderService = new();
+
+    private readonly CustomerDiscountBaseService _customerDiscountBaseService = new();
+    private readonly DayOfTheWeekFactorService _dayOfTheWeekFactorService = new();
+
+    public double CalculateDiscountPercentage(int customerId)
+    {
+        if(!_orderService.HasEnoughOrders(customerId))
+        {
+            return 0;
+        }
+
+        return _customerDiscountBaseService.CalculateDiscountBase(customerId)
+            * _dayOfTheWeekFactorService.CalculateDayOfTheWeekFactor();
+    }
+}
---
+++ DesignPatterns/Structural/Facade/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Facade;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var facade = new DiscountFacade();
+
+        Console.WriteLine(facade.CalculateDiscountPercentage(1));
+        Console.WriteLine(facade.CalculateDiscountPercentage(10));
+    }
+}
