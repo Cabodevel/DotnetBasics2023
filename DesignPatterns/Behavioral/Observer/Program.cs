0c1a0277f52177e15bb4e6ee888cb8eef1ec8a47
Carlos <20067643+Cabodevel@users.noreply.github.com>
2023-10-12 11:04:15 +0200

    State

---
+++ DesignPatterns/Behavioral/Observer/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Observer;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        TicketStockService ticketStockService = new();
+        TicketResellerService ticketResellerService = new();
+        OrderService orderService = new();
+
+        // add two observers
+        orderService.AddObserver(ticketStockService);
+        orderService.AddObserver(ticketResellerService);
+
+        // notify
+        orderService.CompleteTicketSale(1, 2);
+
+        // remove one observer
+        orderService.RemoveObserver(ticketResellerService);
+
+        // notify
+        orderService.CompleteTicketSale(2, 4);
+    }
+}
